b323158358a635cb6492583c560011ec528db4ac
Shiv Bharthur <shiv.bharthur@gmail.com>
2011-09-26 14:54:28 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EstoreAttire::Application.config.secret_token = 'ec139c4373b3ae961bf6f228e39589edc95f5a2b0fe85f16d0fda8cf11ab706033b9ba5d82b8f06fa8eb58746cc780b605e14eabb0c772363406681e11fdb05e'
